bc1f0e6264ef0e191ea594065ed9ab5549e41328
itsfrncsA <francisarillo@gmail.com>
2026-07-10 15:14:02 +0800

    fix: disable my_nui_resource client script and ui_page to prevent double menu overlap

--- my_nui_resource/fxmanifest.lua
+++ my_nui_resource/fxmanifest.lua
@@ -5,8 +5,8 @@ author 'Frncs'
 description 'Shinigami Esse - Enhanced NUI Menu v2.0'
 version '2.0.0'
 
-client_script 'client.lua'
-ui_page 'ui/index.html'
+-- This resource is now a pure asset container for the DUI.
+-- client_script and ui_page are removed to avoid double menu conflicts.
 
 files {
     'ui/index.html',
